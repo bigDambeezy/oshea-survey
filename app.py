021b10293e7109e7cfe6e1e87d375f799187e381
Daniel Edikan <114314592+bigDambeezy@users.noreply.github.com>
2023-07-19 22:34:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ def load_lottieurl(url):
 
 # Use Local CSS
 def local_css(file_name):
-    with open(file_name) as f:
+    with open(streamlit-trial-page) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css('style/style.css.css')
+local_css('style/style.css')
 
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://assets10.lottiefiles.com/private_files/lf30_wqypnpu5.json")
