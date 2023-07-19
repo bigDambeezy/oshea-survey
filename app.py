59ffada45befe3a621e1daa7fc4982a58167e542
Daniel Edikan <114314592+bigDambeezy@users.noreply.github.com>
2023-07-19 22:10:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ st.set_page_config(page_title="Edikan Webpage", page_icon=":mage:", layout="wide
 
 def load_lottieurl(url):
     r = requests.get(url)
-    if r.status_code != 200:
-        return None
     return r.json()
 
 
